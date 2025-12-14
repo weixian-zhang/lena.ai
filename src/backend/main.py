b5fcc47785796f3468dab62e0b6fd5de3d21ba9b
Weixian Zhang <weixianzhang@outlook.com>
2025-12-14 23:02:30 +0800

    fixed az_shell to execute multiple commands with stdout and stderr read for each command

---
+++ src/backend/main.py
@@ -0,0 +1,38 @@
+from tools.az_shell import AzShell, AzShellResult
+from tools.azcli_cmd_tool import AzCliTool, AzCliToolResult
+from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential
+load_dotenv()
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    prompt_1 = """
+            create a virtual network named 'myVNet' in resource group 'myResourceGroup' with address prefix 172.15.0.0/16 and a subnet named 'mySubnet' with address prefix 172.15.1.0/24.
+            Create a virtual machine named 'myVM' in resource group 'myResourceGroup' with UbuntuLTS image and Standard_DS1_v2 size and in virtual network myVNet.
+             """
+    
+    prompt_2 = "update Storage account with name 'strgcwhd', set Allow storage account key access to true."
+
+    async def main():
+        
+        azcli_tool_instance = AzCliTool()
+        result: AzCliToolResult = await azcli_tool_instance.ainvoke(
+            {'prompt': prompt_2}
+        )
+
+        if result.success:
+
+            shell = AzShell()
+
+            for cmd in result.az_commands:
+                
+                shell_result: AzShellResult = await shell.run(cmd, timeout=2)
+
+                if shell_result.success:
+                    print(f"Command output:\n{shell_result.stdout}")
+                else:
+                    print(f"Command failed with error:\n{shell_result.stderr}")
+
+    asyncio.run(main())
