12491a30e137ab9372b148f35e1cf28577aa23fe
Weixian Zhang <weixianzhang@outlook.com>
2025-12-18 16:34:56 +0800

    feat: added write_to_file tool to smol CodeAgent

--- src/backend/tools/langchain_code.py
+++ src/backend/tools/langchain_code.py
@@ -1,21 +1,54 @@
-from smolagents import OpenAIServerModel, DuckDuckGoSearchTool, CodeAgent
+from smolagents import OpenAIServerModel, DuckDuckGoSearchTool, CodeAgent, ToolCall
 from langchain_core.tools import BaseTool
 from pydantic import BaseModel, Field
-from typing import Type
-import os
+from typing import Type, Any, List
+import os, sys
+from tools.smol_write_file import SmolWriteFileTool
 from dotenv import load_dotenv
 load_dotenv()
+
+# Get the absolute path of the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, parent_dir)
+from config import Config
+
+config = Config()
 
 class CodeToolInput(BaseModel):
     prompt: str = Field(..., description="The prompt to generate code for") 
 
+class CodeAgentToolCall(BaseModel):
+    name: str = Field(..., description="name of tool")
+    arg: str = Field(..., description="generated code snippet to execute")
 
 class CodeToolResult(BaseModel):
     success: bool = Field(default=False, description="Indicates whether the code generation and execution was successful.")
-    result: str = Field(default='', description="The output from executing the generated code.")
+    output: Any = Field(default=None, description="The final output from the code execution.")
+    messages: List = Field(default=[], description="The messages exchanged during the code generation and execution process.")
+    tool_calls: List = Field(default=[], description="all tool calls made during code generation and execution.")
     error: str = Field(default='', description="Error message if the code generation or execution failed.")
 
+code_agent_prompt_templates = {
+    # "system_prompt": """
+    # You are a highly skilled Python programmer and code executor.
+    # Your task is to generate Python code snippets based on user prompts and execute them to obtain results.
+    # You have access to a set of <tools> to assist you in this process.
+    
+    # <tools>
+    # 1. DuckDuckGoSearchTool: Use this tool to search the web for information that can help you generate accurate and effective code snippets.
+    # 2. read_file_tool: Use this tool to read text content, code, or data from a file.
+    # 3. write_file_tool: Use this tool to write text content, code, or data to a file.
+    # 4. list_files_tool: Use this tool to list files in a directory.
+    
+    # """,
 
+    "final_answer": """
+    use the following format for final answer:
+    {
+        "output": <final output from code execution>
+    }
+    """
+}
 class CodeTool(BaseTool):
     """
     This tool is a wrapper around SmolAgents CodeAgent to generate and execute code snippets based on a given prompt.
@@ -43,34 +76,83 @@ class CodeTool(BaseTool):
         """Generate code snippet from the given prompt."""
         raise NotImplementedError("Synchronous code generation is not implemented.")
 
+
     async def _arun(self, prompt: str) -> str:
         """Asynchronous version of the code generator and executor"""
 
-        deployment_name = os.getenv("OPENA_AI_MODEL_DEPLOYMENT_NAME")
-        api_url = os.getenv("FOUNDRY_ENDPOINT")
-        api_key = os.getenv("AZURE_OPENAI_API_KEY")
+        try:
 
-        llm = OpenAIServerModel(
-            model_id=deployment_name,
-            api_base=api_url,
-            api_key=api_key
-        )
+            deployment_name = os.getenv("OPENA_AI_MODEL_DEPLOYMENT_NAME")
+            api_url = os.getenv("FOUNDRY_ENDPOINT")
+            api_key = os.getenv("AZURE_OPENAI_API_KEY")
 
-        code_agent = CodeAgent(model=llm, tools=[DuckDuckGoSearchTool()])
+            llm = OpenAIServerModel(
+                model_id=deployment_name,
+                api_base=api_url,
+                api_key=api_key
+            )
 
-        stream_generator = code_agent.run(prompt, stream=True)
-        for response in stream_generator:
-            print(response)  # Iterate to the end to get the final response
+            code_agent = CodeAgent(model=llm, 
+                                   use_structured_outputs_internally=True,
+                                   prompt_templates= code_agent_prompt_templates,
+                                   tools=[DuckDuckGoSearchTool(), SmolWriteFileTool()])
 
-        return response.output
+            tool_calls = []
+            messages = []
+            output: Any = None
+
+            stream_generator = code_agent.run(prompt, stream=True)
+            for response in stream_generator:
+                
+                if self._has_messages(response):
+                    messages = response.model_input_messages
+
+                if self._is_tool_call(response):
+                    tool_calls.append(CodeAgentToolCall(
+                        name=response.name,
+                        arg=response.arguments
+                    ))
+                
+                if self._has_output(response):
+                    output = response.output
+
+                print(response)  # Iterate to the end to get the final response
+
+            return CodeToolResult(
+                success=True,
+                output=output,
+                messages=messages,
+                tool_calls=tool_calls,
+                error='')
+        
+        except Exception as e:
+            return CodeToolResult(
+                success=False,
+                output=None,
+                error=str(e))
+        
+    def _is_tool_call(self, obj: any) -> bool:
+        return isinstance(obj, ToolCall)
     
+    def _has_messages(self, obj: any) -> bool:
+        return hasattr(obj, 'model_input_messages')
+    
+    def _has_output(self, obj: any) -> bool:
+        return hasattr(obj, 'output')
+
 
 # test tool
 if __name__ == "__main__":
     import asyncio
 
     code_tool = CodeTool()
-    prompt = "Write a Python function to calculate the factorial of number 5, run it and print result."
+    prompt_1 = "Write a Python function to calculate the factorial of number 5, run it and print result."
+
+    write_to_file_prompt_1 = f"""
+     build an app that does factorial for a given number.
+     Write the app to a file name 'factorial.py'. The root folder to write to is: {config.cwd_dir}""
+     Write the following content to a file named 'factorial.py':
+    """
     
-    result = asyncio.run(code_tool._arun(prompt))
+    result = asyncio.run(code_tool._arun(prompt_1))
     print(result)
