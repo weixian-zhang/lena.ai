e2e2f55913ff92bd89837504d4361ffaf709f08d
Weixian Zhang <weixianzhang@outlook.com>
2025-12-23 18:02:53 +0800

    feat(value_resolver_agent): added value resolver agent to update original prompt with missing values

--- src/backend/agents/workflow.py
+++ src/backend/agents/workflow.py
@@ -1,4 +1,5 @@
 from langgraph.graph import StateGraph, START, END
+from langgraph.graph.state import CompiledStateGraph, StateT, ContextT, InputT, OutputT
 from langchain_core.messages import HumanMessage
 from state import ExecutionState
 from value_resolver_agent import ValueResolverAgent
@@ -9,7 +10,7 @@ class AzureWorkflow:
     def __init__(self):
         self.state: ExecutionState = ExecutionState()
         self.value_resolver_agent = ValueResolverAgent()
-        self.workflow = StateGraph(ExecutionState)
+        self.workflow = CompiledStateGraph[StateT, ContextT, InputT, OutputT]
     
     def _build_graph(self):
 
@@ -19,16 +20,28 @@ class AzureWorkflow:
         
         self.workflow.add_edge(START, "check_for_missing_azure_values")
         self.workflow.add_conditional_edges(
-            "check_for_missing_azure_values", self.value_resolver_agent.need_human_fill_missing_values, 
+            "check_for_missing_azure_values", self.value_resolver_agent.need_human_to_fill_missing_values,
             {
-                "need_human_fill_missing_values": self.value_resolver_agent.check_with_human_on_missing_values, 
-                "no_need_human_fill_missing_values": END
+                "need_human_to_fill_missing_values":  'check_with_human_on_missing_values', 
+                "no_need_human_to_fill_missing_values": END
              }
         )
-        return self.workflow.compile()
+        # after human fills in missing values, re-check for missing values
+        self.workflow.add_edge("check_with_human_on_missing_values", 'check_for_missing_azure_values')
+        self.workflow = self.workflow.compile()
     
 
-    def execute(self, user_prompt: str) -> dict:
+    def invoke(self, user_prompt: str) -> dict:
 
-        self.state.scratchpad.user_prompt = user_prompt
+        self.state.scratchpad.original_prompt = user_prompt
         return self.workflow.invoke(self.state)
+    
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+    
+    azure_workflow = AzureWorkflow()
+    azure_workflow._build_graph()
+    result = azure_workflow.invoke("Create an Azure VM with 4 CPUs and 16GB RAM in East US region.")
+    print("Final Result:", result)
